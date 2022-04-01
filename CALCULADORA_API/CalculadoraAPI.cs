4502a59f95e61b1dd1adf9010e672869e56af343
Alexsander <alexsandergutierreez@gmail.com>
2022-03-31 21:29:27 -0300

    First commit

---
+++ CALCULADORA_API/CalculadoraAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULADORA_API
+{
+    public class CalculadoraAPI
+    {
+        public static double somar(double a, double b) {
+            return a + b;
+        }
+
+        public static double subtrair(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double dividir(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
